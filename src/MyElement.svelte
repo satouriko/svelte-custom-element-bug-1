4e3477beba6afbda9e585e594c0aaecf5146a3e3
Li,Jiahao <cuteloli@tencent.com>
2024-08-10 17:55:21 +0800

    init

---
+++ src/MyElement.svelte
@@ -0,0 +1,14 @@
+<svelte:options
+  customElement={{
+    tag: 'my-custom-element',
+  }}
+/>
+
+<button><slot /></button>
+
+<style>
+  button {
+    display: block;
+    width: 100%;
+  }
+</style>
---
+++ src/RootElement.svelte
@@ -0,0 +1,14 @@
+<svelte:options
+  customElement={{
+    tag: 'root-custom-element',
+    props: {},
+  }}
+/>
+
+<slot />
+
+<style>
+  :host {
+    display: block;
+  }
+</style>
